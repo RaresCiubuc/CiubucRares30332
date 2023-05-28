bebdd06b49d6c391366bc713bee72a7a4f2477c7
RaresCiubuc <130379605+RaresCiubuc@users.noreply.github.com>
2023-05-28 23:44:56 +0300

    Add files via upload

---
+++ ExecutionThread.java
@@ -0,0 +1,55 @@
+package Lab4.Exercise1;
+
+public class ExecutionThread extends Thread{
+    private int activityMin, activityMax;
+    private int sleep;
+    private Integer monitor;
+    private boolean isRunning;
+
+    public ExecutionThread(Integer monitor, int activityMin, int activityMax, int sleep){
+        this.activityMin = activityMin;
+        this.activityMax = activityMax;
+        this.sleep = sleep;
+        this.monitor = monitor;
+        isRunning = true;
+    }
+
+    public void run(){
+        System.out.println(this.getName() + " - STATE1");
+
+        if(this.getName().equals("Thread-0")){
+            synchronized (monitor){
+                System.out.println(this.getName() + " - STATE2");
+                int k = (int) Math.round(Math.random() * (activityMax - activityMin) + activityMin);
+                for(int i = 0; i <= k * 1000000; i++){
+                    i++;
+                    i--;
+                }
+            }
+            System.out.println(this.getName() + " - STATE3");
+        }
+
+        if(this.getName().equals("Thread-1")){
+            try{
+                this.sleep(sleep*50);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println(this.getName() + " - STATE2");
+            synchronized (monitor){
+                System.out.println(this.getName() + " - STATE3");
+                int k = (int) Math.round(Math.random() * (activityMax - activityMin) + activityMin);
+                for(int i = 0; i <= k * 1000000; i++){
+                    i++;
+                    i--;
+                }
+            }
+//            while(isRunning) {
+                System.out.println(this.getName() + " - STATE4");
+
+                System.out.println(this.getName() + " - STATE5");
+//            }
+        }
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+package Lab4.Exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        int monitor = 0;
+
+        new ExecutionThread(monitor, 3,6, 0).start();
+        new ExecutionThread(monitor, 1,5, 3).start();
+    }
+
+
+}
